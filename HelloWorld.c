18a78e064795f701940d9367987aaa300930fbae
johnny <johnnyjohnny6201@gmail.com>
2020-03-13 21:06:26 +0800

    newbee

---
+++ HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+
+    for (i = 0; i < 10; i++) {
+        printf("Hello world %d times! \n", i);
+    }
+
+    return 0;
+}
---
+++ introduction.py
@@ -0,0 +1,4 @@
+def main():
+    for i in range (1, 10):
+        print('I am Johnny.', i, 'times!')
+main()
